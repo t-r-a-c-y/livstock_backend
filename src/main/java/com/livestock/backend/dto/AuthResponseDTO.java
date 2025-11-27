b2c6d2b87f06333fde5feb1d81dc6b75fe486569
t-r-a-c-y <tracytesi69@gmail.com>
2025-11-27 17:17:22 +0200

    Updated backend structure + added new features

--- src/main/java/com/livestock/backend/dto/AuthResponseDTO.java
+++
@@ -1,9 +0,0 @@
-package com.livestock.backend.dto;
-
-import lombok.Data;
-
-@Data
-public class AuthResponseDTO {
-    private String token;
-    private String refreshToken;
-}
--- src/main/java/com/livestock/backend/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.livestock.backend.exception;
-
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
