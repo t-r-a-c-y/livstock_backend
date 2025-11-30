b3c52efa9cef5cceda959841d285ee17ff5ac481
t-r-a-c-y <tracytesi69@gmail.com>
2025-11-30 21:03:49 +0200

    added backend files and fixed structure

---
+++ Livestock/backend/livstock_backend/livestock-management-backend/src/test/java/com/livestock/LivestockManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livestock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivestockManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
