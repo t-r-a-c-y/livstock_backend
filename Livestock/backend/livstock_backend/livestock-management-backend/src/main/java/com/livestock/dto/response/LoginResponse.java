b0f0bfa69858d0bd76132ba33e505bea7e92de9e
t-r-a-c-y <tracytesi69@gmail.com>
2025-12-29 13:36:22 +0200

    debugging

--- Livestock/backend/livstock_backend/livestock-management-backend/src/main/java/com/livestock/dto/response/LoginResponse.java
+++ Livestock/backend/livstock_backend/livestock-management-backend/src/main/java/com/livestock/dto/response/LoginResponse.java
@@ -13,7 +13,18 @@ public class LoginResponse {
     private String role;
     private String avatar;
     private String token;
+
+
+    public LoginResponse(String token) {
+            this.token = token;
+        }
+        public String getToken() {
+            return token;
+        }
+
     public void setToken(String token) {
         this.token = token;
     }
+
+
 }
