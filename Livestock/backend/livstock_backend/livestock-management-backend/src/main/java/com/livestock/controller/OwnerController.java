389f91524cac84bf7ea5fe33c06f701b6f990b90
t-r-a-c-y <tracytesi69@gmail.com>
2026-01-17 19:39:44 +0200

    debugging

--- Livestock/backend/livstock_backend/livestock-management-backend/src/main/java/com/livestock/controller/OwnerController.java
+++ Livestock/backend/livstock_backend/livestock-management-backend/src/main/java/com/livestock/controller/OwnerController.java
@@ -3,7 +3,6 @@ package com.livestock.controller;
 import com.livestock.dto.request.OwnerRequest;
 import com.livestock.dto.response.OwnerResponse;
 import com.livestock.dto.response.ApiResponse;
-import com.livestock.entity.Owner;
 import com.livestock.service.OwnerService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
