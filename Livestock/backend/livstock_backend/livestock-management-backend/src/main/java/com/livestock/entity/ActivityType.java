8ac7187b09c827779e69b629158eee35426af569
t-r-a-c-y <tracytesi69@gmail.com>
2026-01-19 14:06:55 +0200

    renew

---
+++ Livestock/backend/livstock_backend/livestock-management-backend/src/main/java/com/livestock/entity/ActivityType.java
@@ -0,0 +1,13 @@
+package com.livestock.entity;
+
+public enum ActivityType {
+    VACCINATION,
+    FEEDING,
+    BREEDING,
+    TREATMENT,
+    MILKING,
+    SALE,
+    BIRTH,
+    DEATH,
+    OTHER
+}
