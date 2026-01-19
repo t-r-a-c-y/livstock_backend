3732759372f755375aa2d6a5084d5bcea4d93c4c
t-r-a-c-y <tracytesi69@gmail.com>
2026-01-19 14:07:15 +0200

    renew

---
+++ Livestock/backend/livstock_backend/livestock-management-backend/src/main/java/com/livestock/entity/NotificationType.java
@@ -0,0 +1,10 @@
+package com.livestock.entity;
+
+public enum NotificationType {
+    HEALTH_ALERT,
+    BREEDING_REMINDER,
+    VACCINATION_DUE,
+    FINANCIAL_ALERT,
+    SYSTEM_UPDATE,
+    USER_ACTIVITY
+}
