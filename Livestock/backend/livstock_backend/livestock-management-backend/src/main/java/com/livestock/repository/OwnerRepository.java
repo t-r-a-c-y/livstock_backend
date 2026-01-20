973152237a20e913192dc7a6f04f25ca54f0e2b7
t-r-a-c-y <tracytesi69@gmail.com>
2026-01-20 07:04:53 +0200

    renew

--- Livestock/backend/livstock_backend/livestock-management-backend/src/main/java/com/livestock/repository/OwnerRepository.java
+++ Livestock/backend/livstock_backend/livestock-management-backend/src/main/java/com/livestock/repository/OwnerRepository.java
@@ -5,6 +5,7 @@ import com.livestock.entity.Owner;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
