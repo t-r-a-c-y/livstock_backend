01b4fef2a851960d78cc54c7d51f2e462334d7dc
t-r-a-c-y <tracytesi69@gmail.com>
2026-01-21 09:13:42 +0200

    renew

--- Livestock/backend/livstock_backend/livestock-management-backend/src/main/java/com/livestock/dto/LoginRequestDto.java
+++ Livestock/backend/livstock_backend/livestock-management-backend/src/main/java/com/livestock/dto/LoginRequestDto.java
@@ -1,9 +1,16 @@
 package com.livestock.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class LoginRequestDto {
+
+    @NotBlank
+    @Email
     private String email;
+
+    @NotBlank
     private String password;
 }
