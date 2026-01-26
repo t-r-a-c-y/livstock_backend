a9f44cc07a16ff0eb86cbd0ba16c7c0e8df28d86
t-r-a-c-y <tracytesi69@gmail.com>
2026-01-26 10:06:33 +0200

    all the thing

---
+++ Livestock/backend/livstock_backend/livestock-management-backend/src/main/java/com/livestock/exception/JsonbMapConverter.java
@@ -0,0 +1,30 @@
+package com.livestock.exception;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+import java.util.Map;
+
+@Converter(autoApply = true)
+public class JsonbMapConverter implements AttributeConverter<Map<String, Object>, String> {
+
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    @Override
+    public String convertToDatabaseColumn(Map<String, Object> attribute) {
+        try {
+            return mapper.writeValueAsString(attribute);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Error converting map to JSON", e);
+        }
+    }
+
+    @Override
+    public Map<String, Object> convertToEntityAttribute(String dbData) {
+        try {
+            return mapper.readValue(dbData, Map.class);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Error converting JSON to map", e);
+        }
+    }
+}
